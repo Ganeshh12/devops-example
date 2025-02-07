7e90dcb5e8a3b08e5c07988e8775646a5685d46b
Ganesh <gg450169@gmail.com>
2025-02-07 15:01:17 +0530

    api

--- convert.js
+++ convert.js
@@ -1,9 +1,9 @@
-const EXCHANGE_RATE = 83.0;
-const convert = inr => {
- if (isNaN(inr)) {
- throw "Invalid amount";
- } else {
- return (inr / EXCHANGE_RATE).toFixed(2);
- }
-}
-module.exports = convert;
+const EXCHANGE_RATE = 83.0; 
+const convert = inr => { 
+    if (isNaN(inr)) { 
+        throw "Invalid amount"; 
+    } else { 
+        return (inr / EXCHANGE_RATE).toFixed(2); 
+    } 
+} 
+module.exports = convert; 
